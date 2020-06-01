27bde603b76702541ce65252ce2306b821f659c3
Andrey Side <sideandrey@gmail.com>
2020-06-01 12:32:13 +0300

    0106

---
+++ Miles.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String [] args) {
+        int ticketprice = 12150;
+        int miles = 20;
+        int total = ticketprice/miles;
+        System.out.println(total);
+
+    }
+}
